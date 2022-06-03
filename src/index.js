f9a797975b301ca984cb40c9b9f29f8e23c1cafd
Giovanni Robira <grobira@twilio.com>
2022-06-03 12:00:48 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import * as FlexPlugin from '@twilio/flex-plugin';
+
+import AdvancedMessagingEventsPlugin from './AdvancedMessagingEventsPlugin';
+
+FlexPlugin.loadPlugin(AdvancedMessagingEventsPlugin);
