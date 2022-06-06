f4dc3952a7b323235e37ec6e26cf75eb4f4fd63b
Tom Horton <thomasphorton@gmail.com>
2022-06-06 17:15:27 -0400

    Adds some additional logic around determining internal/external messages.

--- src/utils/tests/getKeyMessage.test.js
+++ src/utils/tests/getKeyMessage.test.js
@@ -1,24 +1,34 @@
-const getKeyMessage = require('../getKeyMessage');
-const testData = require('./testData');
+import getKeyMessage from '../getKeyMessage';
+import testData from './testData';
 
 test ('empty message array', () => {
-    expect(getKeyMessage(testData.emptyMessageArray, testData.memberMap)).toBe(undefined)
+    expect(
+        getKeyMessage(
+            testData.emptyMessageArray,
+            testData.memberMap
+        )).toBe(undefined)
 })
 
 test ('single message array', () => {
-    expect(getKeyMessage(testData.singleMessageArray, testData.memberMap)).toBe(
-        testData.singleMessageArray[0]
-    )
+    expect(
+        getKeyMessage(
+            testData.singleMessageArray,
+            testData.memberMap
+        )).toBe(testData.singleMessageArray[0])
 })
 
 test ('two messages from same user', () => {
-    expect(getKeyMessage(testData.twoMessagesFromSameUserArray, testData.memberMap)).toBe(
-        testData.twoMessagesFromSameUserArray[0]
-    )
+    expect(
+        getKeyMessage(
+            testData.twoMessagesFromSameUserArray,
+            testData.memberMap
+        )).toBe(testData.twoMessagesFromSameUserArray[0])
 })
 
-test ('back and forth', () => {
-    expect(getKeyMessage(testData.backAndForthMessageArray, testData.memberMap)).toBe(
-        testData.backAndForthMessageArray[3]
-    )
+test ('back and forth conversation', () => {
+    expect(
+        getKeyMessage(
+            testData.backAndForthMessageArray,
+            testData.memberMap
+        )).toBe(testData.backAndForthMessageArray[3])
 })
