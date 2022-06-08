daa240985b712e67ebc52e4dfa7660653daa6e63
Tom Horton <thomasphorton@gmail.com>
2022-06-08 11:08:15 -0400

    Changes isInternalMessage to isCustomerMessage, inverts output

--- src/util/tests/isInternalMessage.test.js
+++ src/util/tests/isInternalMessage.test.js
@@ -1,9 +1,9 @@
-import isInternalMessage from '../isInternalMessage';
+import isCustomerMessage from '../isCustomerMessage';
 import testData from './testData'
 
 test('no message', () => {
   expect(
-    isInternalMessage(
+    isCustomerMessage(
       {}
     )
   ).toBe(undefined);
@@ -14,11 +14,11 @@ test ('internal message with external role sid set in .env', () => {
   process.env.CUSTOMER_ROLE_SIDS = testData.customerRoleSidsString;
 
   expect(
-    isInternalMessage(
+    isCustomerMessage(
       testData.backAndForthMessageArrayNoIsFromMe[1],
       testData.memberMap
     )
-  ).toBe(true)
+  ).toBe(false)
 })
 
 test ('external message with external role sid set in .env', () => {
@@ -26,11 +26,11 @@ test ('external message with external role sid set in .env', () => {
   process.env.CUSTOMER_ROLE_SIDS = testData.customerRoleSidsString;
 
   expect(
-    isInternalMessage(
+    isCustomerMessage(
       testData.backAndForthMessageArrayNoIsFromMe[0],
       testData.memberMap
     )
-  ).toBe(false)
+  ).toBe(true)
 })
 
 test ('internal message with external role sid in the config', () => {
@@ -38,12 +38,12 @@ test ('internal message with external role sid in the config', () => {
   delete process.env.CUSTOMER_ROLE_SIDS;
 
   expect(
-    isInternalMessage(
+    isCustomerMessage(
       testData.backAndForthMessageArrayNoIsFromMe[1],
       testData.memberMap,
       {customerRoleSids: testData.customerRoleSidsString}
     )
-  ).toBe(true)
+  ).toBe(false)
 })
 
 test ('external message with external role sid in the config', () => {
@@ -51,12 +51,12 @@ test ('external message with external role sid in the config', () => {
   delete process.env.CUSTOMER_ROLE_SIDS;
 
   expect(
-    isInternalMessage(
+    isCustomerMessage(
       testData.backAndForthMessageArrayNoIsFromMe[0],
       testData.memberMap,
       {customerRoleSids: testData.customerRoleSidsString}
     )
-  ).toBe(false)
+  ).toBe(true)
 })
 
 test ('internal message fallback to isFromMe', () => {
@@ -64,11 +64,11 @@ test ('internal message fallback to isFromMe', () => {
   delete process.env.CUSTOMER_ROLE_SIDS;
 
   expect(
-    isInternalMessage(
+    isCustomerMessage(
       testData.backAndForthEndingWithAgentMessageArray[1],
       testData.memberMap
     )
-  ).toBe(true)
+  ).toBe(false)
 })
 
 test ('external message fallback to isFromMe', () => {
@@ -76,9 +76,9 @@ test ('external message fallback to isFromMe', () => {
   delete process.env.CUSTOMER_ROLE_SIDS;
 
   expect(
-    isInternalMessage(
+    isCustomerMessage(
       testData.backAndForthEndingWithAgentMessageArray[0],
       testData.memberMap
     )
-  ).toBe(false)
+  ).toBe(true)
 })
