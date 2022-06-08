59b6c8151efee4376e178ac2d833d17b69b565f3
Giovanni Robira <grobira@twilio.com>
2022-06-08 17:23:45 -0300

    Fixing typo

--- src/flex-hooks/Actions/CustomActions.js
+++ src/flex-hooks/Actions/CustomActions.js
@@ -1,7 +1,7 @@
 import * as Flex from '@twilio/flex-ui';
 
 export default (flex, manager) => {
-    flex.Actions.registerAction("UrgentMode", (payload) => {
+    flex.Actions.registerAction("UrgencyMode", (payload) => {
         alert("UrgentMode ")
     })
     flex.Actions.registerAction("StaleMode", (payload) => {
