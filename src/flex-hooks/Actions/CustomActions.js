f7a334477954229c5b944978dbf7c02c32311c3a
Giovanni Robira <grobira@twilio.com>
2022-06-09 15:25:34 -0300

    adding action name to handler payload

--- src/flex-hooks/Actions/CustomActions.js
+++ src/flex-hooks/Actions/CustomActions.js
@@ -1,6 +1,10 @@
 import { SilentActionHandler } from "../../util/actionsUtil";
 
 export default (flex, manager) => {
-    flex.Actions.registerAction("UrgencyMode", SilentActionHandler);
-    flex.Actions.registerAction("StaleMode", SilentActionHandler);
+    flex.Actions.registerAction("UrgencyMode", (payload) => {
+        SilentActionHandler({ ...payload, action: "UrgencyMode" })
+    });
+    flex.Actions.registerAction("StaleMode", (payload) => {
+        SilentActionHandler({ ...payload, action: "StaleMode" })
+    });
 }
