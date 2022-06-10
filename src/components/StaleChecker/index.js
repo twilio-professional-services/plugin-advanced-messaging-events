5ea9b9c419d984d811503ae2c0c7a98392db7021
John Fischelli <john.fischelli@gmail.com>
2022-06-10 17:44:57 -0500

    fixed implementation

--- src/components/StaleChecker/index.js
+++ src/components/StaleChecker/index.js
@@ -13,17 +13,35 @@ const modes = {
     URGENCY_MODE: {
         name: URGENCY_MODE,
         actionName: "UrgencyMode",
-        timerThreshold: ONE_SECOND * 6
+        timerThreshold: ONE_MINUTE
     },
     STALE_MODE: {
         name: STALE_MODE,
         actionName: "StaleMode",
-        timerThreshold: ONE_SECOND * 10
+        timerThreshold: ONE_MINUTE
     },
     DEFAULT_MODE: {
         name: DEFAULT_MODE,
         actionName: "DefaultMode"
     }
+};
+
+// Calculates Ms Since Key Message
+const getMsSinceKeyMessage = (keyMessage) => {
+    const now = new Date().getTime();
+    const messageTime = new Date(keyMessage?.source?.timestamp).getTime()
+    return Math.floor((now - messageTime));
+}
+
+// Function to verify the current status of the chat
+// Get the message timestamp and compare with the current timestamp
+// Check if the threshold have been reached for both cases (agent and customer)
+// if none conditions matched, return DefaultMode
+const checkMode = (keyMessage, isFromCustomer) => {
+    if (!keyMessage) {
+        return modes.DEFAULT_MODE;
+    }
+    return isFromCustomer ? modes.URGENCY_MODE : modes.STALE_MODE;
 }
 
 function StaleChecker({task, chatChannel}) {
@@ -37,66 +55,48 @@ function StaleChecker({task, chatChannel}) {
     }
 
     const taskStatus = task.status;
-    const { keyMessage, isFromCustomer } = getKeyMessage(messages, members);
+    const { keyMessage } = getKeyMessage(messages, members);
 
-    const getMsSinceKeyMessage = () => {
-        const now = new Date().getTime();
-        const messageTime = new Date(keyMessage?.source?.timestamp).getTime()
-        return Math.floor((now - messageTime));
-    }
+    if (!keyMessage) return null;
 
     const [canFireAction, setCanFireAction] = useState(true);
-    const [chatMode, setChatMode] = useState(modes.DEFAULT_MODE);
-    const [msSinceKeyMessage, setMsSinceKeyMessage] = useState(getMsSinceKeyMessage());
-
-    // Function to verify the current status of the chat
-    // Get the message timestamp and compare with the current timestamp
-    // Check if the threshold have been reached for both cases (agent and customer)
-    // if none conditions matched, return DefaultMode
-    const checkMode = (keyMessage, isFromCustomer) => {
-        if (!keyMessage) {
-            return modes.DEFAULT_MODE;
-        }
-        return isFromCustomer ? modes.URGENCY_MODE : modes.STALE_MODE;
-    }
 
     // set a timer that runs every second
     // cleans itself up once the task enters wrapping
     useEffect(() => {
         let intervalId;
+
         const cleanup = () => clearInterval(intervalId);
 
         if (taskStatus === TASK_STATUS_WRAPPING) {
-            setMsSinceKeyMessage(0);
             return cleanup;
         }
 
         intervalId = setInterval(() => {
-            setMsSinceKeyMessage(getMsSinceKeyMessage());
+            const { keyMessage, isFromCustomer } = getKeyMessage(messages, members);
+            const currentMode = checkMode(keyMessage, isFromCustomer);
+
+            // if default mode - there is no timerThreshold
+            if (currentMode.name === DEFAULT_MODE) return;
+
+            // check if we've already fired our action
+            if (!canFireAction) return;
+
+            if (getMsSinceKeyMessage(keyMessage) >= currentMode.timerThreshold) {
+                Flex.Actions.invokeAction(currentMode.actionName, { task, chatChannel })
+                console.log(currentMode.actionName.toUpperCase())
+                // action fired, set this to false so we dont constantly fire the action
+                setCanFireAction(false);
+            }
         }, ONE_SECOND);
 
         return cleanup;
-    }, [taskStatus, keyMessage]);
+    }, [taskStatus, messages, members, canFireAction]);
 
-    // check the mode everytime the keymessage changes, or the isFromCustomer changes
+    // when the key message changes, we can fire new actions
     useEffect(() => {
-        const currentMode = checkMode(keyMessage, isFromCustomer);
-        setChatMode(currentMode)
-        // keymessage changed - that means we reset our fireAction state
         setCanFireAction(true);
-    }, [keyMessage, isFromCustomer]);
-
-    // every second, check to see if we should fire our action
-    useEffect(() => {
-        if (chatMode.name === DEFAULT_MODE) return;
-        if (!canFireAction) return;
-
-        if (msSinceKeyMessage >= chatMode.timerThreshold) {
-            Flex.Actions.invokeAction(chatMode.actionName, { task, chatChannel })
-            // action fired, set this to false so we dont constantly fire the action
-            setCanFireAction(false);
-        }
-    }, [msSinceKeyMessage, chatMode, task, chatChannel])
+    }, [keyMessage])
 
     return (<div />);
 }
--- src/util/actionsUtil.js
+++ src/util/actionsUtil.js
@@ -1,12 +1,3 @@
 export const SilentActionHandler = async (payload) => {
-    try {
-        payload.task || payload.channel
-            ? console.log(`${payload.action} executed with task and channel information`)
-            : console.log(
-                `Task or channel information is missing for the ${payload.action} action`
-            );
-    } catch (err) {
-        console.log(err);
-        throw err();
-    }
+    return
 };
