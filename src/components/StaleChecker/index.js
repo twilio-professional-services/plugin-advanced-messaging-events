3bbea6f5fb6779f6644309e5174d3fc1eaca5eae
Giovanni Robira <grobira@twilio.com>
2022-06-22 21:41:57 -0300

    Stalecomponent merge/hotfix (#4)
    * fixing interval loop not working as expected
    
    * fixing action not defined
    
    * adding action name to handler payload
    
    * Improving readbility
    
    * Fix reversed modes
    
    Co-authored-by: Sam Johnson <sam@scj.me>

--- src/components/StaleChecker/index.js
+++ src/components/StaleChecker/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import * as Flex from '@twilio/flex-ui';
+import getKeyMessage from '../../util/getKeyMessage';
+
 
 const URGENCY_MODE = "URGENCY_MODE";
 const STALE_MODE = "STALE_MODE";
@@ -24,38 +26,27 @@ const modes = {
 function StaleChecker(props) {
 
     const [chatMode, setChatMode] = useState(modes.DEFAULT_MODE);
-
-    function getKeyMessage(channel) {
-        return {
-            keyMessage: {
-                source: {
-                    timestamp: "2022-06-08T19:09:01.937Z"
-                }
-            },
-            isInternal: true
-        }
-    }
-
+    const [clock, setClock] = useState(true);
 
     // useEffect with [] param should behave as ComponentDidMount
     // Here we start a loop to get the key message and check the current mode. At the end we only update the React State if the mode has changed
     useEffect(() => {
         const interval = setInterval(() => {
-            verifyChatMode()
+            setClock(clock => !clock)
         }, 2000);
 
         return () => clearInterval(interval);
     }, [])
 
-    function verifyChatMode() {
+    useEffect(() => {
         if (props.chatChannel) {
-            const keyMessage = getKeyMessage(props.chatChannel.messages, props.chatChannel.members)
-
-            const currentMode = checkMode(keyMessage);
-
-            setChatMode(currentMode)
+            const message = getKeyMessage(props.chatChannel.messages, props.chatChannel.members)
+            if (message.keyMessage) {
+                const currentMode = checkMode(message);
+                setChatMode(currentMode)
+            }
         }
-    }
+    }, [clock])
 
     // useEffect with [chatMode] param should behave as ComponentDidUpdate, but should run only if chatMode have been updated
     // Here we check if the new Mode is not the Default, since only Urgent and Stale mode should fire an Action
@@ -70,19 +61,20 @@ function StaleChecker(props) {
     // Get the message timestamp and compare with the current timestamp
     // Check if the threshold have been reached for both cases (agent and customer)
     // if none conditions matched, return DefaultMode
-    function checkMode({ keyMessage, isInternal }) {
+    function checkMode({ keyMessage, isFromCustomer }) {
         const now = new Date().getTime();
         const messageTime = new Date(keyMessage.source.timestamp).getTime()
-        const timeSinceKeyMessage = now - messageTime;
+        const minutesSinceKeyMessage = Math.floor((now - messageTime) / 60000);
 
-        if (isInternal) {
-            if (timeSinceKeyMessage > modes.URGENCY_MODE.timerThreshold) {
+        if (isFromCustomer) {
+            if (minutesSinceKeyMessage > modes.URGENCY_MODE.timerThreshold) {
                 return modes.URGENCY_MODE
             }
         } else {
-            if (timeSinceKeyMessage > modes.STALE_MODE.timerThreshold) {
+            if (minutesSinceKeyMessage > modes.STALE_MODE.timerThreshold) {
                 return modes.STALE_MODE
             }
+
         }
 
         return modes.DEFAULT_MODE;
--- src/util/actionsUtil.js
+++ src/util/actionsUtil.js
@@ -1,9 +1,9 @@
 export const SilentActionHandler = async (payload) => {
     try {
         payload.task || payload.channel
-            ? console.log(`${action} executed with task and channel information`)
+            ? console.log(`${payload.action} executed with task and channel information`)
             : console.log(
-                `Task or channel information is missing for the ${action} action`
+                `Task or channel information is missing for the ${payload.action} action`
             );
     } catch (err) {
         console.log(err);
